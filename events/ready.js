4de0a22f45c486c5b8cd13a081cdce35480f4b27
Joaquim Peixoto <jhcpeixoto@outlook.pt>
2021-04-08 09:52:15 +0100

    DISCORD BOT

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,17 @@
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-
-    client.user.setActivity(client.config.discord.activity);
+  console.log(
+    `Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`
+  );
+  setInterval(() => {
+    client.user
+      .setPresence({
+        game: {
+          name: client.guilds.cache.size + " servers using!",
+          type: "WATCHING",
+        },
+        status: "online",
+      })
+      .then(console.log)
+      .catch(console.error);
+  }, 1000 * 60 * 5);
 };
